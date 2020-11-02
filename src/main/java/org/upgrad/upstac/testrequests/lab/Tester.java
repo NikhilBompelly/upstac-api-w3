5dc20c68e468e4c99586954a036aea3973fcf4af
NikhilBompelly <sunny4nikhil@gmail.com>
2020-11-02 06:15:36 +0530

    Initial commit

---
+++ src/main/java/org/upgrad/upstac/testrequests/lab/Tester.java
@@ -0,0 +1,4 @@
+package org.upgrad.upstac.testrequests.lab;
+
+public class Tester {
+}
---
+++ src/test/java/org/upgrad/upstac/UpstacApplicationTests.java
@@ -0,0 +1,13 @@
+package org.upgrad.upstac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UpstacApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
